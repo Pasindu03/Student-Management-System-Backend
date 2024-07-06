81235ce497fd0da28953fd885e5d88d9534f613a
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-06 10:44:18 +0530

    Dto Files creation

---
+++ src/main/java/org/example/sms/dto/Student.java
@@ -0,0 +1,18 @@
+package org.example.sms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student implements Serializable {
+    private String id;
+    private String name;
+    private String email;
+    private String city;
+    private String level;
+}
