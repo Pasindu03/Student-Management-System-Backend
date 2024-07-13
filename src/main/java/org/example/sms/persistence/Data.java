81cc8ea5ffc2216722fd49e80c5756e4c206971d
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-13 15:28:33 +0530

    Data Process Interface

--- src/main/java/org/example/sms/persistence/Data.java
+++ src/main/java/org/example/sms/persistence/Data.java
@@ -5,9 +5,9 @@ import org.example.sms.dto.StudentDTO;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public interface Data {
+public  interface Data {
     StudentDTO getStudent(String id , Connection connection) throws SQLException;
     String saveStudent(StudentDTO dto, Connection connection) throws SQLException;
-    boolean deleteStudent(String id, Connection connection)throws SQLException;
+    String deleteStudent(String id, Connection connection)throws SQLException;
     boolean updateStudent(String id, StudentDTO dto, Connection connection)throws SQLException;
 }
