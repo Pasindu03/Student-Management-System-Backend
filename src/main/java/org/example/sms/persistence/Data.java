17840b768d936e39af257f0f7cd8383a8694e451
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-13 12:44:23 +0530

    Data Interface - Persistence

---
+++ src/main/java/org/example/sms/persistence/Data.java
@@ -0,0 +1,13 @@
+package org.example.sms.persistence;
+
+import org.example.sms.dto.StudentDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface Data {
+    StudentDTO getStudent(String id , Connection connection) throws SQLException;
+    String saveStudent(StudentDTO dto, Connection connection) throws SQLException;
+    boolean deleteStudent(String id, Connection connection)throws SQLException;
+    boolean updateStudent(String id, StudentDTO dto, Connection connection)throws SQLException;
+}
