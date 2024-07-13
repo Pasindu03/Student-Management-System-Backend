fdee65f6ee796c57c434f666132e18d4b8a31933
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-13 15:29:24 +0530

    sealed

--- src/main/java/org/example/sms/persistence/Data.java
+++ src/main/java/org/example/sms/persistence/Data.java
@@ -5,7 +5,7 @@ import org.example.sms.dto.StudentDTO;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public  interface Data {
+public sealed interface Data permits DataProcess {
     StudentDTO getStudent(String id , Connection connection) throws SQLException;
     String saveStudent(StudentDTO dto, Connection connection) throws SQLException;
     String deleteStudent(String id, Connection connection)throws SQLException;
